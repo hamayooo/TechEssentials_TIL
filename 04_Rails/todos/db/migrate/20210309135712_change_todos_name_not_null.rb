a99fa2312664d548f205ade4bff4ddc9ee1908d5
hamayooo <hamayo@floto.co.jp>
2026-08-25 17:54:31 +0900

    chore: 練習用Railsアプリを削除（master.key・credentials・sqliteを含んでいたため）

--- 04_Rails/todos/db/migrate/20210309135712_change_todos_name_not_null.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTodosNameNotNull < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null :todos, :name, false
-  end
-end
--- 04_Rails/todos/db/migrate/20210309150300_change_todos_name_limit30.rb
+++
@@ -1,8 +0,0 @@
-class ChangeTodosNameLimit30 < ActiveRecord::Migration[5.2]
-  def up
-    change_column :todos, :name, :string, limit: 30
-  end
-  def down
-    change_column :todos, :name, :string
-  end
-end
